16293b1ad56136b67cbac3faf844eac34c405cd7
Octal <59418903+oliver-makes-code@users.noreply.github.com>
2022-11-10 14:01:08 -0600

    a

---
+++ main.ts
@@ -0,0 +1,7 @@
+import  "./pkg/wasos.js"
+import init from "./pkg/wasos.js"
+
+const mod = await init()
+
+
+console.log(mod.test())
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn test() -> String {
+    "Test!".to_string()
+}
